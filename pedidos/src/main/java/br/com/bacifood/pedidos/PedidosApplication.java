1e8c196658c87073ce6888c9f50610a0570aa099
italo-baciliere <italoluciano57@gmail.com>
2023-02-22 15:59:49 -0300

    Adicionado o ms de pedidos

---
+++ pedidos/src/main/java/br/com/bacifood/pedidos/PedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.bacifood.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+}
